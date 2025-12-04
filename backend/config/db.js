3a01950bd9380f43d1c608bdb134a06fe42fa556
Bangó Máté <bangomate2443@PETOFI-ASZOD.LAN>
2025-12-04 12:38:13 +0100

    dotenv files and configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+const host = process.env.DATABASE_HOST;
+const database = process.env.DATABASE_NAME;
+const user = process.env.DATABASE_USER;
 
 const pool = mysql.createPool({
-    host: "localhost",
-    database: "videoteka",
-    user: "root",
-    password: "",
+    host: host,
+    database: database,
+    user: user,
+    password: "", 
 });
 
 export default pool;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "../routes/authRoutes.js";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.BACKEND_PORT;
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
 app.use("/", authRoutes);
 
-const PORT = 3000;
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server started on: ${PORT}.`);
 })
